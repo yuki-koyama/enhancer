adc0b06aac81a09cf9ba320403d15d5729179648
Yuki Koyama <yuki@koyama.xyz>
2019-03-05 22:25:30 +0900

    Add minimal function definitions

--- include/enhancer/enhancerwidget.hpp
+++ include/enhancer/enhancerwidget.hpp
@@ -1,9 +1,26 @@
 #ifndef enhancerwidget_hpp
 #define enhancerwidget_hpp
 
+#include <QOpenGLFunctions>
+#include <QOpenGLWidget>
+#include <QImage>
+
 namespace enhancer
 {
+    class EnhancerWidget : public QOpenGLWidget, protected QOpenGLFunctions
+    {
+    public:
+        EnhancerWidget(QWidget* parent = nullptr);
+        void setImage(const QImage& image) { image_ = image; }
 
+    protected:
+        void initializeGL() override;
+        void paintGL() override;
+        void resizeGL(int width, int height) override;
+
+    private:
+        QImage image_;
+    };
 }
 
 #endif /* enhancerwidget_hpp */
--- src/enhancerwidget.cpp
+++ src/enhancerwidget.cpp
@@ -1,1 +1,24 @@
 #include <enhancer/enhancerwidget.hpp>
+#include <QOpenGLShaderProgram>
+#include <QOpenGLTexture>
+
+namespace enhancer
+{
+    EnhancerWidget::EnhancerWidget(QWidget* parent) :
+    QOpenGLWidget(parent)
+    {
+    }
+
+    void EnhancerWidget::initializeGL()
+    {
+        initializeOpenGLFunctions();
+    }
+
+    void EnhancerWidget::paintGL()
+    {
+    }
+
+    void EnhancerWidget::resizeGL(int width, int height)
+    {
+    }
+}
