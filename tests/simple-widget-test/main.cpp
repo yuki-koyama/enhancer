ce3cb0ca210a43edec726f3f191fc045d1c5edba
Yuki Koyama <yuki@koyama.xyz>
2019-03-06 17:59:26 +0900

    Add some lines for settings OpenGL 3.2 on macOS

--- tests/simple-widget-test/main.cpp
+++ tests/simple-widget-test/main.cpp
@@ -4,6 +4,14 @@
 int main(int argc, char** argv)
 {
     QApplication app(argc, argv);
+
+#if defined(__APPLE__)
+    QSurfaceFormat format;
+    format.setVersion(3, 2);
+    format.setProfile(QSurfaceFormat::CoreProfile);
+    QSurfaceFormat::setDefaultFormat(format);
+#endif
+
     enhancer::EnhancerWidget widget;
     widget.show();
     return app.exec();
